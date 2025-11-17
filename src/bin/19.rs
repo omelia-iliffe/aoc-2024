494e30c230d444e217782abde20188dd7d98baad
Omelia Iliffe <omelia.iliffe@wetaworkshop.co.nz>
2025-11-17 15:05:33 +1300

    cargo fmt

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -1,58 +1,73 @@
 use memoize::memoize;
 use nom::bytes::complete::tag;
 use nom::character::complete::{alpha1, line_ending};
-use nom::{IResult, Parser};
 use nom::multi::separated_list1;
 use nom::sequence::{pair, preceded, terminated};
+use nom::{IResult, Parser};
 
 advent_of_code::solution!(19);
 
 fn parse(input: &str) -> IResult<&str, (Vec<String>, Vec<String>)> {
     pair(
-        terminated(separated_list1(tag(", "), alpha1.map(String::from)), line_ending),
-        preceded(line_ending, separated_list1(line_ending, alpha1.map(String::from)))
+        terminated(
+            separated_list1(tag(", "), alpha1.map(String::from)),
+            line_ending,
+        ),
+        preceded(
+            line_ending,
+            separated_list1(line_ending, alpha1.map(String::from)),
+        ),
     )(input)
 }
 
 #[memoize]
 fn recursive_check(pattern: String, towels: Vec<String>) -> bool {
-    towels.iter()
+    towels
+        .iter()
         .map(|ref towel| {
-            pattern.strip_prefix(towel.as_str()).is_some_and(|r_pattern| {
-                r_pattern.is_empty() || recursive_check(r_pattern.to_string(), towels.clone())
-            })
-        }).any(|v| v)
+            pattern
+                .strip_prefix(towel.as_str())
+                .is_some_and(|r_pattern| {
+                    r_pattern.is_empty() || recursive_check(r_pattern.to_string(), towels.clone())
+                })
+        })
+        .any(|v| v)
 }
-
 
 #[memoize]
 fn recursive_find(pattern: String, towels: Vec<String>) -> usize {
-    towels.iter().filter_map(|towel| {
-        pattern.strip_prefix(towel.as_str()).map(|r_pattern| {
-            if r_pattern.is_empty() {
-                Some(1)
-            } else {
-                Some(recursive_find(r_pattern.to_string(), towels.clone()))
-            }
-        }).flatten()
-    }).sum()
+    towels
+        .iter()
+        .filter_map(|towel| {
+            pattern
+                .strip_prefix(towel.as_str())
+                .map(|r_pattern| {
+                    if r_pattern.is_empty() {
+                        Some(1)
+                    } else {
+                        Some(recursive_find(r_pattern.to_string(), towels.clone()))
+                    }
+                })
+                .flatten()
+        })
+        .sum()
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
     let (_, (towels, patterns)) = parse(input).unwrap();
-    let n = patterns.iter().filter(|pattern| {
-        recursive_check(pattern.to_string(), towels.clone())
-    }
-    ).count();
+    let n = patterns
+        .iter()
+        .filter(|pattern| recursive_check(pattern.to_string(), towels.clone()))
+        .count();
     Some(n as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
     let (_, (towels, patterns)) = parse(input).unwrap();
-    let n: usize = patterns.iter().map(|pattern| {
-        recursive_find(pattern.to_string(), towels.clone())
-    }
-    ).sum();
+    let n: usize = patterns
+        .iter()
+        .map(|pattern| recursive_find(pattern.to_string(), towels.clone()))
+        .sum();
     Some(n as u64)
 }
 
